9633851843c35d7057bcb7775c8106617f6bdb20
Raim Sargado <raimsargado@gmail.com>
2017-09-09 03:13:48 +0800

    Added new build apk

--- app/src/main/java/com/simpleapp/todo/ToDo.java
+++ app/src/main/java/com/simpleapp/todo/ToDo.java
@@ -1,24 +1,28 @@
 package com.simpleapp.todo;
 
 
-
 class ToDo {
     private String mTodo;
     private String mTodoId;
     private String mDoneUndone;
 
-    ToDo(String todo, String todoId,String doneUndone) {
+    ToDo(String todo, String todoId, String doneUndone) {
         mTodo = todo;
         mTodoId = todoId;
         mDoneUndone = doneUndone;
     }
 
+
     String getTodo() {
         return mTodo;
     }
 
-    String getTodoId() { return mTodoId; }
+    String getTodoId() {
+        return mTodoId;
+    }
 
-    String getDoneUndone() { return mDoneUndone; }
+    String getDoneUndone() {
+        return mDoneUndone;
+    }
 
 }
